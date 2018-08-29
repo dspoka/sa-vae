97801378b2d912bf369d6a4335211f455f0850a5
Daniel Spokoyny <dspokoyn@saskia.ml.cmu.edu>
2018-08-29 02:50:36 -0400

    added file to reproduce and fixed the .4 version errors

---
+++ reproduce.py
@@ -0,0 +1,138 @@
+import argparse
+import preprocess_text
+import train_text
+
+def preprocess_config():
+    config = argparse.Namespace()
+    config.trainfile = '/remote/bones/user/dspokoyn/vae-mode-collapse/datasets/yahoo/data_yahoo_release/train.txt'
+    config.valfile = '/remote/bones/user/dspokoyn/vae-mode-collapse/datasets/yahoo/data_yahoo_release/valid.txt'
+    config.testfile = '/remote/bones/user/dspokoyn/vae-mode-collapse/datasets/yahoo/data_yahoo_release/test.txt'
+    config.outputfile = 'data/yahoo/yahoo'
+    # config.vocabsize = 70000
+    config.vocabsize = 11
+    config.vocabminfreq = -1
+    # config.batchsize = 32
+    config.batchsize = 256
+    # config.seqlength = 200
+    config.seqlength = 50
+    config.vocabfile = ''
+    config.shuffle = 1
+    return config
+    #     python preprocess_text.py --trainfile data/yahoo/train.txt --valfile data/yahoo/val.txt
+    # --testfile data/yahoo/test.txt --outputfile data/yahoo/yahoo
+
+def train_config():
+    config = argparse.Namespace()
+    config.train_file = 'data/yahoo/yahoo-train.hdf5'
+    config.val_file = 'data/yahoo/yahoo-val.hdf5'
+    config.test_file = 'data/yahoo/yahoo-test.hdf5'
+    config.train_from = ''
+    config.gpu = 1
+    # checkpoint_path model-path
+
+    # # Model options
+    config.latent_dim =32
+    config.enc_word_dim =512
+    config.enc_h_dim =1024
+    config.enc_num_layers =1
+    config.dec_word_dim =512
+    config.dec_h_dim =1024
+    config.dec_num_layers =1
+    config.dec_dropout =0.5
+    config.train_n2n =1
+    config.train_kl =1
+
+    # # Optimization options
+    config.checkpoint_path ='baseline.pt'
+    config.slurm =0
+    config.warmup =10
+    # config.num_epochs =30
+    # config.min_epochs =15
+    config.num_epochs =3
+    config.min_epochs =1
+    config.start_epoch =0
+    config.svi_steps =20
+    config.svi_lr1 =1
+    config.svi_lr2 =1
+    config.eps =1e-5
+    config.decay =0
+    config.momentum =0.5
+    config.lr =1
+    config.max_grad_norm =5
+    config.svi_max_grad_norm =5
+    config.seed =3435
+    config.print_every=100
+    config.test = 0
+
+    return config
+
+def autoreg_default(config):
+    config.model = 'autoreg'
+    return config
+
+def vae_default(config):
+    config.model = 'vae'
+    return config
+
+def svi_default(config):
+    config.model = 'svi'
+    config.svi_steps = 20
+    config.train_n2n = 0
+    return config
+
+def vae_svi_default(config):
+    config.model = 'savae'
+    config.svi_steps = 20
+    config.train_n2n = 0
+    config.train_kl = 0
+    return config
+
+def vae_svi_kl_default(config):
+    config.model = 'savae'
+    config.svi_steps = 20
+    config.train_n2n = 0
+    config.train_kl = 1
+    return config
+
+def sa_vae_default(config):
+    config.model = 'savae'
+    config.svi_steps = 20
+    config.train_n2n = 1
+    return config
+
+
+# Autoregressive (i.e. language model): --model autoreg
+# VAE: --model vae
+
+# SVI: --model svi --svi_steps 20 --train_n2n 0
+
+# VAE+SVI: --model savae --svi_steps 20 --train_n2n 0 --train_kl 0
+# VAE+SVI+KL: --model savae --svi_steps 20 --train_n2n 0 --train_kl 1
+# SA-VAE: --model savae --svi_steps 20 --train_n2n 1
+
+
+
+if __name__ == '__main__':
+    prep_config = preprocess_config()
+    preprocess_text.get_data(prep_config)
+    tr_config = train_config()
+    # tr_config.mode = 'autoreg'
+    # tr_config.mode = 'vae'
+    # tr_config.mode = 'svi'
+    # tr_config.mode = 'vae_svi'
+    # tr_config.mode = 'vae_svi_kl'
+    tr_config.mode = 'sa_vae'
+    if tr_config.mode == 'autoreg':
+        tr_config =  autoreg_default(tr_config)
+    elif tr_config.mode == 'vae':
+        tr_config =  vae_default(tr_config)
+    elif tr_config.mode == 'svi':
+        tr_config =  svi_default(tr_config)
+    elif tr_config.mode == 'vae_svi':
+        tr_config =  vae_svi_default(tr_config)
+    elif tr_config.mode == 'vae_svi_kl':
+        tr_config =  vae_svi_kl_default(tr_config)
+    elif tr_config.mode == 'sa_vae':
+        tr_config =  sa_vae_default(tr_config)
+
+    train_text.main(tr_config)
--- train_text.py
+++ train_text.py
@@ -77,7 +77,9 @@ def main(args):
   print('Val data: %d batches' % len(val_data))
   print('Word vocab size: %d' % vocab_size)
   if args.slurm == 0:
-    cuda.set_device(args.gpu)
+    # cuda.set_device(args.gpu)
+    gpu_id = 0
+    device = torch.device(f"cuda:{gpu_id}" if torch.cuda.is_available() else "cpu")
   if args.train_from == '':
     model = RNNVAE(vocab_size = vocab_size,
                    enc_word_dim = args.enc_word_dim,
@@ -89,13 +91,13 @@ def main(args):
                    dec_dropout = args.dec_dropout,
                    latent_dim = args.latent_dim,
                    mode = args.model)
-    for param in model.parameters():    
-      param.data.uniform_(-0.1, 0.1)      
+    for param in model.parameters():
+      param.data.uniform_(-0.1, 0.1)
   else:
     print('loading model from ' + args.train_from)
     checkpoint = torch.load(args.train_from)
     model = checkpoint['model']
-      
+
   print("model architecture")
   print(model)
 
@@ -105,10 +107,12 @@ def main(args):
     args.beta = 1.
   else:
     args.beta = 0.1
-    
+
   criterion = nn.NLLLoss()
-  model.cuda()
-  criterion.cuda()
+  # model.cuda()
+  # criterion.cuda()
+  model.to(device)
+  criterion.to(device)
   model.train()
 
   def variational_loss(input, sents, model, z = None):
@@ -120,17 +124,17 @@ def main(args):
     return nll + args.beta*kl
 
   update_params = list(model.dec.parameters())
-  meta_optimizer = OptimN2N(variational_loss, model, update_params, eps = args.eps, 
+  meta_optimizer = OptimN2N(variational_loss, model, update_params, eps = args.eps,
                             lr = [args.svi_lr1, args.svi_lr2],
                             iters = args.svi_steps, momentum = args.momentum,
-                            acc_param_grads= args.train_n2n == 1,  
+                            acc_param_grads= args.train_n2n == 1,
                             max_grad_norm = args.svi_max_grad_norm)
   if args.test == 1:
     args.beta = 1
-    test_data = Dataset(args.test_file)    
-    eval(test_data, model, meta_optimizer)
+    test_data = Dataset(args.test_file)
+    eval(args, test_data, model, meta_optimizer, device)
     exit()
-    
+
   t = 0
   best_val_nll = 1e5
   best_epoch = 0
@@ -138,7 +142,7 @@ def main(args):
   epoch = 0
   while epoch < args.num_epochs:
     start_time = time.time()
-    epoch += 1  
+    epoch += 1
     print('Starting epoch %d' % epoch)
     train_nll_vae = 0.
     train_nll_autoreg = 0.
@@ -149,25 +153,33 @@ def main(args):
     num_sents = 0
     num_words = 0
     b = 0
-    
+
     for i in np.random.permutation(len(train_data)):
       if args.warmup > 0:
         args.beta = min(1, args.beta + 1./(args.warmup*len(train_data)))
-      
+
       sents, length, batch_size = train_data[i]
+      length = length.item()
+      batch_size = batch_size.item()
+
       if args.gpu >= 0:
-        sents = sents.cuda()
+        # sents = sents.cuda()
+        sents = sents.to(device)
+        # batch_size = batch_size.to(device)
       b += 1
-      
+
       optimizer.zero_grad()
       if args.model == 'autoreg':
         preds = model._dec_forward(sents, None, True)
         nll_autoreg = sum([criterion(preds[:, l], sents[:, l+1]) for l in range(length)])
-        train_nll_autoreg += nll_autoreg.data[0]*batch_size
+        train_nll_autoreg += nll_autoreg.item()*batch_size
+        # train_nll_autoreg += nll_autoreg.data[0]*batch_size #old
         nll_autoreg.backward()
-      elif args.model == 'svi':        
-        mean_svi = Variable(0.1*torch.zeros(batch_size, args.latent_dim).cuda(), requires_grad = True)
-        logvar_svi = Variable(0.1*torch.zeros(batch_size, args.latent_dim).cuda(), requires_grad = True)
+      elif args.model == 'svi':
+        # mean_svi = Variable(0.1*torch.zeros(batch_size, args.latent_dim).cuda(), requires_grad = True)
+        # logvar_svi = Variable(0.1*torch.zeros(batch_size, args.latent_dim).cuda(), requires_grad = True)
+        mean_svi = Variable(0.1*torch.zeros(batch_size, args.latent_dim).to(device), requires_grad = True)
+        logvar_svi = Variable(0.1*torch.zeros(batch_size, args.latent_dim).to(device), requires_grad = True)
         var_params_svi = meta_optimizer.forward([mean_svi, logvar_svi], sents,
                                                   b % args.print_every == 0)
         mean_svi_final, logvar_svi_final = var_params_svi
@@ -176,22 +188,24 @@ def main(args):
         nll_svi = sum([criterion(preds[:, l], sents[:, l+1]) for l in range(length)])
         train_nll_svi += nll_svi.data[0]*batch_size
         kl_svi = utils.kl_loss_diag(mean_svi_final, logvar_svi_final)
-        train_kl_svi += kl_svi.data[0]*batch_size      
-        var_loss = nll_svi + args.beta*kl_svi          
+        train_kl_svi += kl_svi.data[0]*batch_size
+        var_loss = nll_svi + args.beta*kl_svi
         var_loss.backward(retain_graph = True)
       else:
         mean, logvar = model._enc_forward(sents)
         z_samples = model._reparameterize(mean, logvar)
         preds = model._dec_forward(sents, z_samples)
         nll_vae = sum([criterion(preds[:, l], sents[:, l+1]) for l in range(length)])
-        train_nll_vae += nll_vae.data[0]*batch_size
+        # train_nll_vae += nll_vae.data[0]*batch_size#old
+        train_nll_vae += nll_vae.item()*batch_size
         kl_vae = utils.kl_loss_diag(mean, logvar)
-        train_kl_vae += kl_vae.data[0]*batch_size        
+        # train_kl_vae += kl_vae.data[0]*batch_size#old
+        train_kl_vae += kl_vae.item()*batch_size
         if args.model == 'vae':
-          vae_loss = nll_vae + args.beta*kl_vae          
+          vae_loss = nll_vae + args.beta*kl_vae
           vae_loss.backward(retain_graph = True)
         if args.model == 'savae':
-          var_params = torch.cat([mean, logvar], 1)        
+          var_params = torch.cat([mean, logvar], 1)
           mean_svi = Variable(mean.data, requires_grad = True)
           logvar_svi = Variable(logvar.data, requires_grad = True)
           var_params_svi = meta_optimizer.forward([mean_svi, logvar_svi], sents,
@@ -202,46 +216,47 @@ def main(args):
           nll_svi = sum([criterion(preds[:, l], sents[:, l+1]) for l in range(length)])
           train_nll_svi += nll_svi.data[0]*batch_size
           kl_svi = utils.kl_loss_diag(mean_svi_final, logvar_svi_final)
-          train_kl_svi += kl_svi.data[0]*batch_size      
-          var_loss = nll_svi + args.beta*kl_svi          
+          train_kl_svi += kl_svi.data[0]*batch_size
+          var_loss = nll_svi + args.beta*kl_svi
           var_loss.backward(retain_graph = True)
           if args.train_n2n == 0:
             if args.train_kl == 1:
               mean_final = mean_svi_final.detach()
-              logvar_final = logvar_svi_final.detach()            
+              logvar_final = logvar_svi_final.detach()
               kl_init_final = utils.kl_loss(mean, logvar, mean_final, logvar_final)
               train_kl_init_final += kl_init_final.data[0]*batch_size
-              kl_init_final.backward(retain_graph = True)              
+              kl_init_final.backward(retain_graph = True)
             else:
               vae_loss = nll_vae + args.beta*kl_vae
               var_param_grads = torch.autograd.grad(vae_loss, [mean, logvar], retain_graph=True)
               var_param_grads = torch.cat(var_param_grads, 1)
-              var_params.backward(var_param_grads, retain_graph=True)              
+              var_params.backward(var_param_grads, retain_graph=True)
           else:
             var_param_grads = meta_optimizer.backward([mean_svi_final.grad, logvar_svi_final.grad],
                                                       b % args.print_every == 0)
             var_param_grads = torch.cat(var_param_grads, 1)
             var_params.backward(var_param_grads)
       if args.max_grad_norm > 0:
-        torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)        
+        torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)
       optimizer.step()
       num_sents += batch_size
       num_words += batch_size * length
-      
+      # num_sents = num_sents.item()
+      # num_words = num_words.item()
       if b % args.print_every == 0:
         param_norm = sum([p.norm()**2 for p in model.parameters()]).data[0]**0.5
-        print('Iters: %d, Epoch: %d, Batch: %d/%d, LR: %.4f, TrainARPPL: %.2f, TrainVAE_PPL: %.2f, TrainVAE_KL: %.4f, TrainVAE_PPLBnd: %.2f, TrainSVI_PPL: %.2f, TrainSVI_KL: %.4f, TrainSVI_PPLBnd: %.2f, KLInitFinal: %.2f, |Param|: %.4f, BestValPerf: %.2f, BestEpoch: %d, Beta: %.4f, Throughput: %.2f examples/sec' % 
-              (t, epoch, b+1, len(train_data), args.lr, np.exp(train_nll_autoreg / num_words), 
+        print('Iters: %d, Epoch: %d, Batch: %d/%d, LR: %.4f, TrainARPPL: %.2f, TrainVAE_PPL: %.2f, TrainVAE_KL: %.4f, TrainVAE_PPLBnd: %.2f, TrainSVI_PPL: %.2f, TrainSVI_KL: %.4f, TrainSVI_PPLBnd: %.2f, KLInitFinal: %.2f, |Param|: %.4f, BestValPerf: %.2f, BestEpoch: %d, Beta: %.4f, Throughput: %.2f examples/sec' %
+              (t, epoch, b+1, len(train_data), args.lr, np.exp(train_nll_autoreg / num_words),
                np.exp(train_nll_vae/num_words), train_kl_vae / num_sents,
                np.exp((train_nll_vae + train_kl_vae)/num_words),
                np.exp(train_nll_svi/num_words), train_kl_svi/ num_sents,
                np.exp((train_nll_svi + train_kl_svi)/num_words), train_kl_init_final / num_sents,
                param_norm, best_val_nll, best_epoch, args.beta,
                num_sents / (time.time() - start_time)))
-          
+
     print('--------------------------------')
     print('Checking validation perf...')
-    val_nll = eval(val_data, model, meta_optimizer)
+    val_nll = eval(args, val_data, model, meta_optimizer, device)
     val_stats.append(val_nll)
     if val_nll < best_val_nll:
       best_val_nll = val_nll
@@ -252,23 +267,29 @@ def main(args):
         'model': model,
         'val_stats': val_stats
       }
-      print('Savaeng checkpoint to %s' % args.checkpoint_path)      
+      print('Savaeng checkpoint to %s' % args.checkpoint_path)
       torch.save(checkpoint, args.checkpoint_path)
-      model.cuda()
+      # model.cuda()
+      model.to(device)
     else:
       if epoch >= args.min_epochs:
         args.decay = 1
     if args.decay == 1:
-      args.lr = args.lr*0.5      
+      args.lr = args.lr*0.5
       for param_group in optimizer.param_groups:
         param_group['lr'] = args.lr
       if args.lr < 0.03:
         break
-      
-def eval(data, model, meta_optimizer):
-    
-  model.eval()
-  criterion = nn.NLLLoss().cuda() 
+
+def eval(args, data, model, meta_optimizer, device):
+  model.dropout.eval()
+  model.dec_linear[0].eval()
+  # model.eval()
+  print(model.dropout.training)
+  print(model.dec_linear[0].training)
+
+  # criterion = nn.NLLLoss().cuda()
+  criterion = nn.NLLLoss().to(device)
   num_sents = 0
   num_words = 0
   total_nll_autoreg = 0.
@@ -279,17 +300,23 @@ def eval(data, model, meta_optimizer):
   best_svi_loss = 0.
   for i in range(len(data)):
     sents, length, batch_size = data[i]
+    length = length.item()
+    batch_size = batch_size.item()
     num_words += batch_size*length
     num_sents += batch_size
     if args.gpu >= 0:
-      sents = sents.cuda()
+      # sents = sents.cuda()
+      sents = sents.to(device)
     if args.model == 'autoreg':
       preds = model._dec_forward(sents, None, True)
       nll_autoreg = sum([criterion(preds[:, l], sents[:, l+1]) for l in range(length)])
-      total_nll_autoreg += nll_autoreg.data[0]*batch_size
+      # total_nll_autoreg += nll_autoreg.data[0]*batch_size #old
+      total_nll_autoreg += nll_autoreg.item()*batch_size
     elif args.model == 'svi':
-      mean_svi = Variable(0.1*torch.randn(batch_size, args.latent_dim).cuda(), requires_grad = True)
-      logvar_svi = Variable(0.1*torch.randn(batch_size, args.latent_dim).cuda(), requires_grad = True)
+      # mean_svi = Variable(0.1*torch.randn(batch_size, args.latent_dim).cuda(), requires_grad = True)
+      # logvar_svi = Variable(0.1*torch.randn(batch_size, args.latent_dim).cuda(), requires_grad = True)
+      mean_svi = Variable(0.1*torch.randn(batch_size, args.latent_dim).to(device), requires_grad = True)
+      logvar_svi = Variable(0.1*torch.randn(batch_size, args.latent_dim).to(device), requires_grad = True)
       var_params_svi = meta_optimizer.forward([mean_svi, logvar_svi], sents)
       mean_svi_final, logvar_svi_final = var_params_svi
       z_samples = model._reparameterize(mean_svi_final.detach(), logvar_svi_final.detach())
@@ -304,9 +331,11 @@ def eval(data, model, meta_optimizer):
       z_samples = model._reparameterize(mean, logvar)
       preds = model._dec_forward(sents, z_samples)
       nll_vae = sum([criterion(preds[:, l], sents[:, l+1]) for l in range(length)])
-      total_nll_vae += nll_vae.data[0]*batch_size
+      # total_nll_vae += nll_vae.data[0]*batch_size#old
+      total_nll_vae += nll_vae.item()*batch_size
       kl_vae = utils.kl_loss_diag(mean, logvar)
-      total_kl_vae += kl_vae.data[0]*batch_size        
+      # total_kl_vae += kl_vae.data[0]*batch_size#old
+      total_kl_vae += kl_vae.item()*batch_size
       if args.model == 'savae':
         mean_svi = Variable(mean.data, requires_grad = True)
         logvar_svi = Variable(logvar.data, requires_grad = True)
@@ -317,8 +346,10 @@ def eval(data, model, meta_optimizer):
         nll_svi = sum([criterion(preds[:, l], sents[:, l+1]) for l in range(length)])
         total_nll_svi += nll_svi.data[0]*batch_size
         kl_svi = utils.kl_loss_diag(mean_svi_final, logvar_svi_final)
-        total_kl_svi += kl_svi.data[0]*batch_size      
+        total_kl_svi += kl_svi.data[0]*batch_size
         mean, logvar = mean_svi_final, logvar_svi_final
+  # num_words = num_words.item()
+  # num_sents = num_sents.item()
   ppl_autoreg = np.exp(total_nll_autoreg / num_words)
   ppl_vae = np.exp(total_nll_vae/ num_words)
   kl_vae = total_kl_vae / num_sents
